2ea0c630ce7a3c1bab239af18b5edd5144c9c06e
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-10-23 20:54:17 +0300

    Enable browser launch on application startup
    Added BrowserLauncher component to open browser on application start.

--- HallOfFame/src/main/java/com/sunmeat/halloffame/HallOfFameApplication.java
+++ HallOfFame/src/main/java/com/sunmeat/halloffame/HallOfFameApplication.java
@@ -4,12 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling // без этой аннотации сервис-фейкомётчик отзывы отправлять не будет
+@EnableScheduling // без цієї анотації планувальник не працюватиме
 @SpringBootApplication
 public class HallOfFameApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HallOfFameApplication.class, args);
 	}
+}
 
+@Component
+class BrowserLauncher {
+	@EventListener(ApplicationReadyEvent.class)
+	public void launchBrowser() {
+		System.setProperty("java.awt.headless", "false"); 
+		var desktop = Desktop.getDesktop();
+		try {
+			desktop.browse(new URI("http://localhost:8081/reviews"));
+		} catch (Exception e) {
+			// нічого не робимо у випадку помилки
+		}
+	}
 }

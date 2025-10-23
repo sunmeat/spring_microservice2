11419105e454f9f05cc8649aacd8958df25f284b
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-10-23 20:49:52 +0300

    Refine comments in RefreshController
    Updated comments in RefreshController to clarify purpose.

--- HallOfFame/src/main/java/com/sunmeat/halloffame/controller/RefreshController.java
+++ HallOfFame/src/main/java/com/sunmeat/halloffame/controller/RefreshController.java
@@ -4,16 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.sunmeat.halloffame.service.FeedbackService;
 import com.sunmeat.halloffame.model.FeedbackMessage;
-
 import java.util.List;
 
-@RestController // с автоматическим обновлением всё сложно, потребовался целый контроллер для формирования объектов-отзывов для аякса
+@RestController // використовується для створення RESTful веб-сервісів
 @RequestMapping("/api/feedbacks")
 public class RefreshController {
-
     @Autowired
     private FeedbackService feedbackService;
 

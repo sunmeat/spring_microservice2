c209b11b8f57e8680b8347a347682e81a990bd77
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-10-23 20:49:20 +0300

    Update feedback list method comment to Ukrainian

--- HallOfFame/src/main/java/com/sunmeat/halloffame/controller/FeedbackController.java
+++ HallOfFame/src/main/java/com/sunmeat/halloffame/controller/FeedbackController.java
@@ -5,17 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.sunmeat.halloffame.service.FeedbackService;
 
 @Controller
 @RequestMapping("/reviews")
 public class FeedbackController {
-
     @Autowired
     private FeedbackService feedbackService;
 
-    @GetMapping  // получает список отзывов и добавляет его в модель для отображения в шаблоне "feedback-list"
+    @GetMapping  // отримує список відгуків і відображає їх на сторінці feedback-list
     public String listReviews(Model model) {
         model.addAttribute("feedbacks", feedbackService.getFeedbacks());
         return "feedback-list";
